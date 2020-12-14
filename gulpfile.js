452ae40785068e7b789213eb5acd765e22908904
Stephenich <sma.gurney@gmail.com>
2020-12-14 14:50:25 +0000

    Added babel dependencies to package json

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const { src, series, parallel, dest, watch } = require('gulp');
 const autoPrefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
 
 function copyHtml() {
     return src('src/*.html')
     .pipe(gulp.dest('docs'));
+}
+
+function jsTask() {
+    return src('src/js/*')
+    .pipe(babel())
+    .pipe(gulp.dest('docs/js'));
 }
 
 function imgTask(){
@@ -20,7 +27,8 @@ function cssTask(){
     .pipe(gulp.dest('docs/css'));
 }
 
-exports.default= parallel(copyHtml, imgTask, cssTask);
+exports.default= parallel(copyHtml, imgTask, cssTask, jsTask);
 exports.copyHtml = copyHtml;
 exports.imgTask =imgTask;
 exports.cssTask = cssTask;
+exports.jsTask = jsTask;
