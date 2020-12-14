26e02ce415e8a66545b3b8f133135a6460164822
Stephenich <sma.gurney@gmail.com>
2020-12-14 14:08:21 +0000

    Added function for image optimisation

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const { src, series, parallel, dest, watch } = require('gulp');
 
 function copyHtml() {
@@ -6,4 +7,12 @@ function copyHtml() {
     .pipe(gulp.dest('docs'));
 }
 
+function imgTask(){
+    return src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('docs/images'));
+}
+
 exports.default= copyHtml;
+exports.copyHtml = copyHtml;
+exports.imgTask =imgTask;
